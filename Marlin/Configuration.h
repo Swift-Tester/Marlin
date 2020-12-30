f077b80a66f0804bbaa54465cb37b06fc7eb0578
Swift-Tester <dmitry.alekseev.ru@gmail.com>
2020-12-30 17:49:25 +0500

    MAXTEMP 300/150

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -458,7 +458,7 @@
 // Above this temperature the heater will be switched off.
 // This can protect components from overheating, but NOT from shorts and failures.
 // (Use MINTEMP for thermistor short/failure protection.)
-#define HEATER_0_MAXTEMP 275
+#define HEATER_0_MAXTEMP 300 // Swift-Tester
 #define HEATER_1_MAXTEMP 275
 #define HEATER_2_MAXTEMP 275
 #define HEATER_3_MAXTEMP 275
@@ -466,7 +466,7 @@
 #define HEATER_5_MAXTEMP 275
 #define HEATER_6_MAXTEMP 275
 #define HEATER_7_MAXTEMP 275
-#define BED_MAXTEMP      125
+#define BED_MAXTEMP      150 // Swift-Tester
 
 //===========================================================================
 //============================= PID Settings ================================
